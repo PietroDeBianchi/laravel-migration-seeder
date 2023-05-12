5cd700ef4e6a6222a82e987eb5bbd87b352ef21a
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-12 18:16:55 +0200

    migration done! yessssss :)

--- database/migrations/2023_05_12_160059_create_trains_table.php
+++ database/migrations/2023_05_12_160059_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("Company", 30);
+            $table->string("Departures_station", 40);
+            $table->string("Arrivals_station", 40);
+            $table->time("Departures_time");
+            $table->time("Arrivals_time");
+            $table->string("Train_code", 8);
+            $table->unsignedTinyInteger("Wagon_numbers");
+            $table->time("Time_lag")->nullable();
+            $table->boolean("Deleted");
             $table->timestamps();
         });
     }
