a29f824eae0b3894a00e51ff8b86c74e677d4092
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-16 21:41:59 +0200

    CSV

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,23 +16,5 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 16; $i++) {
-
-            $train = new Train();
-
-            $train->Company = $faker->word();
-            $train->Date = $faker->date();
-            $train->Departures_station = $faker->city();
-            $train->Arrivals_station = $faker->city();
-            $train->Departures_time = $faker->time("H:i");
-            $train->Arrivals_time = $faker->time("H:i");
-            $train->Train_code = $faker->bothify('??#####');
-            $train->Price = $faker->randomNumber(2, false);
-            $train->Wagon_numbers = $faker->numberBetween(1, 30);
-            $train->Time_lag = $faker->boolean();
-            $train->Deleted = $faker->boolean();
-
-            $train->save();
-        }
     }
 }
